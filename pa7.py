534ec069e2c0ee6664770c07083ee3998bc38675
Chih-Chiang Wei <ccwei@corn11.stanford.edu>
2013-03-05 22:17:34 -0800

    init commit

---
+++ pa7.py
@@ -0,0 +1,58 @@
+import commands
+
+f = open('dic', 'r')
+paragraph = open('paragraph.txt', 'r')
+dic = {}
+for line in f:
+  parts = line.decode('utf-8').rstrip().split(' ')
+  print parts
+  dic[parts[0]] =" ".join(parts[1:])
+f.close()
+
+text = paragraph.read().decode('utf-8').rstrip()
+
+print text.encode('utf-8')
+
+def checkPrefix(dic, word):
+  for w in dic:
+   if w.startswith(word):
+     return True
+
+  return False
+
+idx = 0
+current = 0
+translation = ""
+
+while idx < len(text) - 1:
+  #print 'idx = ', idx, 'current = ', current
+  while(checkPrefix(dic, text[idx:current + 1])):
+    current += 1
+  #print text[idx: current]
+  if text[idx: current] in dic:
+    print text[idx:current].encode('utf-8'), 'in dic'
+    print dic[text[idx: current]].encode('utf-8')
+
+    translation += " " + dic[text[idx: current]]
+
+  if current == idx: #text[idx: current + 1] is not in dictionary
+    idx += 1
+    current += 1
+  else:
+    idx = current #start from text[current: ]
+
+translation = translation.encode('utf-8')
+print translation
+
+f = open('out.txt', 'w')
+f.write(translation)
+f.close()
+
+commands.getoutput('./stanford-postagger.sh ./stanford-postagger/models/english-bidirectional-distsim.tagger ./out.txt > pos.txt')
+
+f = open('pos.txt', 'r')
+pos = f.read()
+print pos
+#print text
+#print len(text)
+#print dic
